02bf0eb169ac96a42fbee292c38d5a4897bb13f3
samchavez <s.jacobs.chavez@gmail.com>
2019-01-17 15:50:44 -0800

    adding sMRI scripts

---
+++ analysis/sMRI/batch_reconall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# This batch file calls on your subject
+# list (named subject_list.txt). And 
+# runs the job_reconall.sh file for 
+# each subject. It saves the ouput
+# and error files in their specified
+# directories.
+#
+# Set your study
+STUDY=/projects/dsnlab/shared/sos_pilot
+
+# Set subject list
+SUBJLIST=`cat subject_list.txt`
+
+# 
+for SUBJ in $SUBJLIST
+do
+ sbatch --export SUBID=${SUBJ} --job-name reconall --partition=short --mem-per-cpu=8G --time=20:00:00 --cpus-per-task=1 -o "${STUDY}"/sos_scripts/sMRI/output/"${SUBJ}"_reconall_output.txt -e "${STUDY}"/sos_scripts/sMRI/output/"${SUBJ}"_reconall_error.txt job_reconall.sh
+done
