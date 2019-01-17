d80b54d3009274f5cf06acb5f2518573e8888cc6
schavez6 <32177888+schavez6@users.noreply.github.com>
2019-01-17 15:25:09 -0800

    changing file names & paths

--- analysis/fMRI/ppc/batch_fmriprep.sh
+++ analysis/fMRI/ppc/batch_fmriprep.sh
@@ -12,10 +12,10 @@
 group_dir=/projects/dsnlab/shared/
 #container=BIDS/SingularityContainers/poldracklab_fmriprep_latest-2017-07-20-dd77d76c5e14.img
 container=BIDS/SingularityContainers/poldracklab_fmriprep_latest-2017-12-07-ba92e815fc4e.img
-study="tag"
+study="sos"
 
 # Set subject list
-SUBJLIST=`cat subject_list_140.txt`
+SUBJLIST=`cat subject_list.txt`
 
 # 
 for SUBJ in $SUBJLIST; do
@@ -23,6 +23,6 @@ for SUBJ in $SUBJLIST; do
 #SUBID=`echo $SUBJ|awk '{print $1}' FS=","`
 #SESSID=`echo $SUBJ|awk '{print $2}' FS=","`
 	
-sbatch --export subid=${SUBJ},group_dir=${group_dir},study=${study},container=${container} --job-name fmriprep --partition=short --time=0-20:00:00 --mem=100G -o "${group_dir}"/"${study}"/TAG_scripts/fMRI/ppc/output/"${SUBJ}"_fmriprep_output.txt -e "${group_dir}"/"${study}"/TAG_scripts/fMRI/ppc/output/"${SUBJ}"_fmriprep_error.txt job_fmriprep.sh
+sbatch --export subid=${SUBJ},group_dir=${group_dir},study=${study},container=${container} --job-name fmriprep --partition=short --time=0-20:00:00 --mem=100G -o "${group_dir}"/"${study}"/SOS_scripts/fMRI/ppc/output/"${SUBJ}"_fmriprep_output.txt -e "${group_dir}"/"${study}"/SOS_scripts/fMRI/ppc/output/"${SUBJ}"_fmriprep_error.txt job_fmriprep.sh
 	
 done
